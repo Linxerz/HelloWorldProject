d8bf665fb75c5c3ef552e502ff17534e912822b7
Linxerz <58645425+Linxerz@users.noreply.github.com>
2020-08-19 15:33:12 -0500

    more practice

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,17 @@ public class Hello {
     public static void main(String[] args)  {
         System.out.println("Hello, Johnathan");
 
-        int myFirstNumber = 5;
-        System.out.println(myFirstNumber);
+        int myFirstNumber = 5 + 5 + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
 
     }
 }
